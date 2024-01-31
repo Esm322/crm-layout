fa9c6abf071f350786559bc4badaf90dddf55a17
Esm322 <86346824+Esm322@users.noreply.github.com>
2024-01-31 16:43:46 +0300

    layout is done

---
+++ build/main.js
@@ -0,0 +1,95 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/img/add-client.svg
+const add_client_namespaceObject = __webpack_require__.p + "./img/add-client.svg";
+;// CONCATENATED MODULE: ./src/img/add-plus.svg
+const add_plus_namespaceObject = __webpack_require__.p + "./img/add-plus.svg";
+;// CONCATENATED MODULE: ./src/img/clear.svg
+const clear_namespaceObject = __webpack_require__.p + "./img/clear.svg";
+;// CONCATENATED MODULE: ./src/img/close.svg
+const close_namespaceObject = __webpack_require__.p + "./img/close.svg";
+;// CONCATENATED MODULE: ./src/img/delete.svg
+const delete_namespaceObject = __webpack_require__.p + "./img/delete.svg";
+;// CONCATENATED MODULE: ./src/img/down.svg
+const down_namespaceObject = __webpack_require__.p + "./img/down.svg";
+;// CONCATENATED MODULE: ./src/img/edit.svg
+const edit_namespaceObject = __webpack_require__.p + "./img/edit.svg";
+;// CONCATENATED MODULE: ./src/img/fb.svg
+const fb_namespaceObject = __webpack_require__.p + "./img/fb.svg";
+;// CONCATENATED MODULE: ./src/img/fio.svg
+const fio_namespaceObject = __webpack_require__.p + "./img/fio.svg";
+;// CONCATENATED MODULE: ./src/img/logo-1024.svg
+const logo_1024_namespaceObject = __webpack_require__.p + "./img/logo-1024.svg";
+;// CONCATENATED MODULE: ./src/img/mail.svg
+const mail_namespaceObject = __webpack_require__.p + "./img/mail.svg";
+;// CONCATENATED MODULE: ./src/img/phone.svg
+const phone_namespaceObject = __webpack_require__.p + "./img/phone.svg";
+;// CONCATENATED MODULE: ./src/img/select-arrow.svg
+const select_arrow_namespaceObject = __webpack_require__.p + "./img/select-arrow.svg";
+;// CONCATENATED MODULE: ./src/img/up.svg
+const up_namespaceObject = __webpack_require__.p + "./img/up.svg";
+;// CONCATENATED MODULE: ./src/img/vk.svg
+const vk_namespaceObject = __webpack_require__.p + "./img/vk.svg";
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/******/ })()
+;
